51204a405b0f2e888587491c798dc2986b9ee152
waffles <waffles@waffless-MacBook-Pro.local>
2014-03-30 19:36:08 -0500

    add main menu

---
+++ whalecombat/MainMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainMenuViewController.h
+//  ChessGame
+//
+//  Created by waffles on 3/30/14.
+//  Copyright (c) 2014 anthony lamantia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainMenuViewController : UIViewController
+
+@end
